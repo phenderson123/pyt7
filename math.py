46af14deafee3deb590e880cb845f4a506b25ca3
Perry E. Henderson <hendersonperry96@yahoo.com>
2017-06-30 15:23:31 -0400

    first commit

---
+++ math.py
@@ -0,0 +1,7 @@
+num = float(input("Enter a number dude..really lol: "))
+if num > 0:
+   print("Positive number... yeah!!!")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number... bummer")
